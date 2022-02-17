ad438cfb079a39dfb58bb5d18c83dfcc0543a7ff
Induwaara Ashinshana <99461853+IT21266232@users.noreply.github.com>
2022-02-17 18:37:24 +0530

    Create Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -4,7 +4,19 @@
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
+   int Mark1,Mark2;
+   float Avg;
+   
+   printf("Enter the marks of first subject:");
+   scanf("%d",&Mark1);
+   
+   printf("Enter the marks of second subject:");
+   scanf("%d",&Mark2);
+   
+   Avg = (Mark1 + Mark2) / 2.0;
+   printf("Your average is:%.2f",Avg); 
   
   return 0;
 }
